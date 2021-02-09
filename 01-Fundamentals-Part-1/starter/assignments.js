130c7223be4955c40270d6dfa81043620be671d7
Mariya <mariya.podosinova@gmail.com>
2021-02-09 15:37:47 +0000

    first commit

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,11 @@
+let country, continent, population, language;
+country = "UK";
+continent = "Europe";
+population = 12000000;
+
+let isIsland = true;
+
+language = "english";
+const brexit_year = 2019;
+
+console.log(isIsland, population, country, language);
