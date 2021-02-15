d33d3ef61858089a0ff369d07dc40ae090cc1b77
Mariya <mariya.podosinova@gmail.com>
2021-02-15 11:53:58 +0000

    refactor the code with the class's solution

--- 03 Fundamentals – Part 2/Challenge1/script.js
+++ 03 Fundamentals – Part 2/Challenge1/script.js
@@ -3,24 +3,26 @@
 // A team ONLY wins if it has at least DOUBLE the average score of the other team. Otherwise, no team wins!
 
 // 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
-function calcAverage(score1, score2, score3) {
-  return score1 + score2 + score3 / 3;
-}
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
 // 2. Use the function to calculate the average for both teams
-const dolphinsAverage = calcAverage(44, 23, 71);
-const koalasAverage = calcAverage(65, 54, 49);
+let dolphinsAverage = calcAverage(44, 23, 71);
+let koalasAverage = calcAverage(65, 54, 49);
 console.log(dolphinsAverage, koalasAverage);
 // 3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'),
 // and then logs the winner to the console, together with the victory points, according to the rule above.
 // Example: "Koalas win (30 vs. 13)".
-function checkWinner(avgDolphins, avgKoalas) {
-  result = `${
-    avgDolphins >= avgKoalas ? "Dolphins" : "Koalas"
-  } win (${avgDolphins} : ${avgKoalas} )`;
-  console.log(result);
-}
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    console.log(`Dolphins win 🏆 (${avgDolphins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    console.log(`Koalas win 🏆 (${avgKoalas} vs. ${avgDolphins})`);
+  } else {
+    console.log("No team wins...");
+  }
+};
 // 4. Use the 'checkWinner' function to determine the winner for both DATA 1 and DATA 2.
-checkWinner(calcAverage(44, 23, 71), calcAverage(65, 54, 49));
+checkWinner(dolphinsAverage, koalasAverage);
 // 5. Ignore draws this time.
 
 // TEST DATA 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
