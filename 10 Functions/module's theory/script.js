9d4c5f4c108e3e2d373ce26811db4033b0a6db5e
Mariya <mariya.podosinova@gmail.com>
2021-03-16 16:42:56 +0000

    add theory for the Closure, part2

--- 10 Functions/module's theory/script.js
+++ 10 Functions/module's theory/script.js
@@ -165,16 +165,53 @@
 // (() => console.log("This will ALSO never run again"))();
 
 //******Closures
-const secureBooking = function () {
-  let passengerCount = 0;
-  return function () {
-    passengerCount++;
-    console.log(`${passengerCount} passengers`);
-  };
+// const secureBooking = function () {
+//   let passengerCount = 0;
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+//******Example 1
+// let f;
+// const g = function () {
+//   const a = 23;
+//   f = function () {
+//     console.log(a * 2);
+//   };
+// };
+// const h = function () {
+//   const b = 777;
+//   f = function () {
+//     console.log(b * 2);
+//   };
+// };
+
+// g();
+// f();
+//******Re-assigning f function
+// h();
+// f();
+// console.dir(f);
+
+//******Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
 };
 
-const booker = secureBooking();
-booker();
-booker();
-
-console.dir(booker);
+const perGroup = 1000;
+boardPassengers(180, 3);
