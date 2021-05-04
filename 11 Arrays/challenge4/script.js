61c45d8a695503474de746f25d96706f4ba9d98a
Mariya Podosinova <mariya.podosinova@gmail.com>
2021-05-04 16:43:57 +0100

    add solution for Challenge4

--- 11 Arrays/challenge4/script.js
+++ 11 Arrays/challenge4/script.js
@@ -18,32 +18,58 @@ the recommended food portion and add it to the object as a new property. Do
 not create a new array, simply loop over the array. Forumla:
 recommendedFood = weight ** 0.75 * 28. (The result is in grams of
 food, and the weight needs to be in kg)*/
+dogs.forEach((dog) => (dog.recFood = Math.trunc(dog.weight ** 0.75 * 28)));
 
 /*2. Find Sarah's dog and log to the console whether it's eating too much or too
 little. Hint: Some dogs have multiple owners, so you first need to find Sarah in
 the owners array, and so this one is a bit tricky (on purpose) 🤓*/
 
+const dogSarah = dogs.find((dog) => dog.owners.includes("Sarah"));
+console.log(dogSarah);
+
 /*3. Create an array containing all owners of dogs who eat too much
 ('ownersEatTooMuch') and an array with all owners of dogs who eat too little
 ('ownersEatTooLittle').*/
+const ownersEatTooMuch = dogs
+  .filter((dog) => dog.curFood > dog.recFood)
+  .map((dog) => dog.owners)
+  .flat();
+console.log(ownersEatTooMuch);
+const ownersEatTooLittle = dogs
+  .filter((dog) => dog.curFood < dog.recFood)
+  .map((dog) => dog.owners)
+  .flat();
+console.log(ownersEatTooLittle);
 
 /*4. Log a string to the console for each array created in 3., like this: "Matilda and
 Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat
 too little!"*/
-
+const message1 = `${ownersEatTooMuch.join(" and ")}'s dogs eat to much`;
+console.log(message1);
+const message2 = `${ownersEatTooLittle.join(" and ")}'s dogs eat
+too little!`;
+console.log(message2);
 /*5. Log to the console whether there is any dog eating exactly the amount of food
 that is recommended (just true or false)*/
-
+const rightAmmount = dogs.some((dog) => dog.curFood === dog.recFood);
+console.log(rightAmmount);
 /*6. Log to the console whether there is any dog eating an okay amount of food
 (just true or false)*/
-
+const okAmmount = dogs.some(
+  (dog) => dog.curFood < dog.recFood * 1.1 && dog.curFood > dog.recFood * 0.9
+);
+console.log(okAmmount);
 /*7. Create an array containing the dogs that are eating an okay amount of food (try
 to reuse the condition used in 6.)*/
-
+const checkEatingOkay = (dog) =>
+  dog.curFood < dog.recFood * 1.1 && dog.curFood > dog.recFood * 0.9;
+console.log(dogs.some(checkEatingOkay));
+console.log(dogs.filter(checkEatingOkay));
 /*8. Create a shallow copy of the 'dogs' array and sort it by recommended food
 portion in an ascending order (keep in mind that the portions are inside the
 array's objects 😉)*/
-
+const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+console.log(dogsSorted);
 /*Hints:
 § Use many different tools to solve these challenges, you can use the summary
 lecture to choose between them 😉
@@ -55,76 +81,76 @@ recommended portion.*/
 /////////////////////////
 // ***Array Method Practice
 // Data
-const account1 = {
-  owner: "Jonas Schmedtmann",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-};
+// const account1 = {
+//   owner: "Jonas Schmedtmann",
+//   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+//   interestRate: 1.2, // %
+//   pin: 1111,
+// };
 
-const account2 = {
-  owner: "Jessica Davis",
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-};
+// const account2 = {
+//   owner: "Jessica Davis",
+//   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+//   interestRate: 1.5,
+//   pin: 2222,
+// };
 
-const account3 = {
-  owner: "Steven Thomas Williams",
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
-};
+// const account3 = {
+//   owner: "Steven Thomas Williams",
+//   movements: [200, -200, 340, -300, -20, 50, 400, -460],
+//   interestRate: 0.7,
+//   pin: 3333,
+// };
 
-const account4 = {
-  owner: "Sarah Smith",
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
-};
+// const account4 = {
+//   owner: "Sarah Smith",
+//   movements: [430, 1000, 700, 50, 90],
+//   interestRate: 1,
+//   pin: 4444,
+// };
 
-const accounts = [account1, account2, account3, account4];
-// 1.
-const bankDepositSum = accounts
-  .flatMap((acc) => acc.movements)
-  .filter((mov) => mov > 0)
-  .reduce((sum, cur) => sum + cur, 0);
-console.log(bankDepositSum);
-// 2.
-const numDeposits1000 = accounts
-  .flatMap((acc) => acc.movements)
-  .filter((mov) => mov >= 1000).length;
-console.log(numDeposits1000);
-//2.1
-const numDeposits1000extra = accounts
-  .flatMap((acc) => acc.movements)
-  .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
-console.log(numDeposits1000extra);
-//3.
-const { deposits, withdrawals } = accounts
-  .flatMap((acc) => acc.movements)
-  .reduce(
-    (sums, cur) => {
-      //cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
-      sums[cur > 0 ? "deposits" : "withdrawals"] += cur;
-      return sums;
-    },
-    { deposits: 0, withdrawals: 0 }
-  );
+// const accounts = [account1, account2, account3, account4];
+// // 1.
+// const bankDepositSum = accounts
+//   .flatMap((acc) => acc.movements)
+//   .filter((mov) => mov > 0)
+//   .reduce((sum, cur) => sum + cur, 0);
+// console.log(bankDepositSum);
+// // 2.
+// const numDeposits1000 = accounts
+//   .flatMap((acc) => acc.movements)
+//   .filter((mov) => mov >= 1000).length;
+// console.log(numDeposits1000);
+// //2.1
+// const numDeposits1000extra = accounts
+//   .flatMap((acc) => acc.movements)
+//   .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
+// console.log(numDeposits1000extra);
+// //3.
+// const { deposits, withdrawals } = accounts
+//   .flatMap((acc) => acc.movements)
+//   .reduce(
+//     (sums, cur) => {
+//       //cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+//       sums[cur > 0 ? "deposits" : "withdrawals"] += cur;
+//       return sums;
+//     },
+//     { deposits: 0, withdrawals: 0 }
+//   );
 
-console.log(deposits, withdrawals);
+// console.log(deposits, withdrawals);
 
-//4.
-const convertTitleCase = function (title) {
-  const capitalize = (str) => str[0].toUpperCase() + str.slice(1);
-  const exceptions = ["a", "an", "the", "but", "or", "on", "in", "with", "and"];
-  const titleCase = title
-    .toLowerCase()
-    .split(" ")
-    .map((word) => (exceptions.includes(word) ? word : capitalize(word)))
-    .join(" ");
-  return capitalize(titleCase);
-};
+// //4.
+// const convertTitleCase = function (title) {
+//   const capitalize = (str) => str[0].toUpperCase() + str.slice(1);
+//   const exceptions = ["a", "an", "the", "but", "or", "on", "in", "with", "and"];
+//   const titleCase = title
+//     .toLowerCase()
+//     .split(" ")
+//     .map((word) => (exceptions.includes(word) ? word : capitalize(word)))
+//     .join(" ");
+//   return capitalize(titleCase);
+// };
 
-console.log(convertTitleCase("this is a nice title"));
-console.log(convertTitleCase("and this is a nice title"));
+// console.log(convertTitleCase("this is a nice title"));
+// console.log(convertTitleCase("and this is a nice title"));
